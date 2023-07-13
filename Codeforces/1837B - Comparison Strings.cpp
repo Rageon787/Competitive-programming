75e7ea1234f986f574e51b265d8b449852f325d6
Ragav Srinivasan <56784771+Rageon787@users.noreply.github.com>
2023-07-13 23:54:03 +0800

    Create 1837B - Comparison Strings.cpp

---
+++ Codeforces/1837B - Comparison Strings.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main()
+{
+	int t; 
+	cin >> t; 
+	while(t--)
+	{
+		int n; 
+		cin >> n; 
+		string s; 
+		cin >> s; 
+		int count = 0; 
+		int ans = 0; 
+		for(int i=1;i<n;i++) 
+		{
+			if(s[i] == s[i - 1]) count++; 
+			else 
+			{
+				ans = max(ans,count); 
+				count = 0; 
+			} 
+		}
+		ans = max(ans,count); 
+		ans+=2;
+		cout << ans << endl; 
+    return 0; 
+	}
+ 
