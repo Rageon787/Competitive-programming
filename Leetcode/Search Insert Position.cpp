9e83f631495326bf201cad4344c0fdbe9df7522c
Ragav Srinivasan <56784771+Rageon787@users.noreply.github.com>
2023-07-14 17:37:51 +0800

    Update Search Insert Position.cpp

--- Leetcode/Search Insert Position.cpp
+++ Leetcode/Search Insert Position.cpp
@@ -1,1 +1,29 @@
-
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        int ans = nums.size(); 
+        while(l <= r)
+        {
+            int mid = (l + r) / 2;
+            if(target < nums[mid])
+            {
+                r = mid - 1;
+                ans = mid; 
+                continue;
+            }
+            if(target > nums[mid])
+            {
+                l = mid + 1;
+                continue;
+            }
+            if(target == nums[mid]) 
+            {
+                ans = mid;
+                break;
+            }
+        }
+        return ans;
+    }
+};
